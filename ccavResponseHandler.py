abeb0177c7df2dddb7d0e41b447db909106e5ce9
arixcod <anandmishra8009@gmail.com>
2024-09-17 12:06:01 +0530

    json response

--- ccavResponseHandler.py
+++ ccavResponseHandler.py
@@ -1,41 +1,31 @@
 #!/usr/bin/python
 
-from ccavutil import encrypt,decrypt
-from string import Template
-
-
-
-
-
-
+import json
+from ccavutil import encrypt, decrypt
 
 
 def res(encResp):
-	'''
-	Please put in the 32 bit alphanumeric key in quotes provided by CCAvenues.
-	'''	 
-	workingKey = '79B8D8AA336EDF95D51C1FA98272E119'
-	decResp = decrypt(encResp,workingKey)
-	data = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'	
-	data = data + decResp.replace('=','</td><td>')
-	data = data.replace('&','</td></tr><tr><td>')
-	data = data + '</td></tr></table>'
-	
-	html = '''\
-	<html>
-		<head>
-			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-			<title>Response Handler</title>
-		</head>
-		<body>
-			<center>
-				<font size="4" color="blue"><b>Response Page</b></font>
-				<br>
-				$response
-			</center>
-			<br>
-		</body>
-	</html>
-	'''
-	fin = Template(html).safe_substitute(response=data)
-	return fin
+    '''
+    Please put in the 32-bit alphanumeric key in quotes provided by CCAvenue.
+    '''
+    workingKey = '79B8D8AA336EDF95D51C1FA98272E119'
+    
+    # Decrypt the encrypted response
+    decResp = decrypt(encResp, workingKey)
+    
+    # Split the decrypted response into key-value pairs
+    key_value_pairs = decResp.split('&')
+    
+    # Create a dictionary to hold the parsed response
+    response_dict = {}
+    
+    # Iterate over each key-value pair and add them to the dictionary
+    for pair in key_value_pairs:
+        if '=' in pair:
+            key, value = pair.split('=')
+            response_dict[key] = value
+    
+    # Convert the dictionary to a JSON response
+    json_response = json.dumps(response_dict, indent=4)
+    
+    return json_response
